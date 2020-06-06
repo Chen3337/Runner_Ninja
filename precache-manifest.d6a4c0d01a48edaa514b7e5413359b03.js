4810abe3d30222a5ae3a7cd85b6e258b1189e429
zhen yong chen <dannychen111@yahoo.com>
2020-06-06 15:37:31 -0500

    Updates

---
+++ precache-manifest.d6a4c0d01a48edaa514b7e5413359b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cc6cefa67e4510e561da1ccfde4939",
+    "url": "/Runner_Ninja/index.html"
+  },
+  {
+    "revision": "ad964167dc6130bedbb0",
+    "url": "/Runner_Ninja/static/js/2.ecb42020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Runner_Ninja/static/js/2.ecb42020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894ffd2fa7ed68d360a3",
+    "url": "/Runner_Ninja/static/js/main.cdd14180.chunk.js"
+  },
+  {
+    "revision": "8496e680bbfd3c6631c4",
+    "url": "/Runner_Ninja/static/js/runtime-main.165f8bcb.js"
+  }
+]);
