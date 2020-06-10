f6b7e6ab514f0bf11ceb84a21c679b90d6d05a1f
zhen yong chen <dannychen111@yahoo.com>
2020-06-09 23:54:16 -0500

    Updates

---
+++ precache-manifest.de7dec3818d740bf3a1b4d159db4980f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73ce2d33bf8c5437498b1d823b513d5",
+    "url": "/Runner_Ninja/index.html"
+  },
+  {
+    "revision": "454b42ca275b7c48e284",
+    "url": "/Runner_Ninja/static/js/2.05a9aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Runner_Ninja/static/js/2.05a9aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a145ada17e8f6b81ab9c",
+    "url": "/Runner_Ninja/static/js/main.27ae3b8d.chunk.js"
+  },
+  {
+    "revision": "8496e680bbfd3c6631c4",
+    "url": "/Runner_Ninja/static/js/runtime-main.165f8bcb.js"
+  },
+  {
+    "revision": "dcab8af0445ff5ef6b80dcfc0b953ba0",
+    "url": "/Runner_Ninja/static/media/Idle__000.dcab8af0.png"
+  },
+  {
+    "revision": "e5ae92f6d6610f33503080ab3712404f",
+    "url": "/Runner_Ninja/static/media/daydreamer.e5ae92f6.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Runner_Ninja/precache-manifest.68e26c36906415a5e701609b61e33f9d.js"
+  "/Runner_Ninja/precache-manifest.de7dec3818d740bf3a1b4d159db4980f.js"
 );
 
 self.addEventListener('message', (event) => {
