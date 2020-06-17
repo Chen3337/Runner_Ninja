52374070caecbbd6c2488d8af48aa3ec219e98bf
zhen yong chen <dannychen111@yahoo.com>
2020-06-16 23:09:54 -0500

    Updates

---
+++ precache-manifest.9e2c5e270e602b6f6513579b7bd7cace.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790177bd93d2ecf3686088f46dfbe6f1",
+    "url": "/Runner_Ninja/index.html"
+  },
+  {
+    "revision": "9f0a9b929f5417ad4fdf",
+    "url": "/Runner_Ninja/static/js/2.583d6559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Runner_Ninja/static/js/2.583d6559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337e2d4867688f5f1e66",
+    "url": "/Runner_Ninja/static/js/main.4f5d72ed.chunk.js"
+  },
+  {
+    "revision": "8496e680bbfd3c6631c4",
+    "url": "/Runner_Ninja/static/js/runtime-main.165f8bcb.js"
+  },
+  {
+    "revision": "e124eec0412519268d2b7671ca0a137f",
+    "url": "/Runner_Ninja/static/media/Glide_000.e124eec0.png"
+  },
+  {
+    "revision": "dcab8af0445ff5ef6b80dcfc0b953ba0",
+    "url": "/Runner_Ninja/static/media/Idle__000.dcab8af0.png"
+  },
+  {
+    "revision": "069af83802c28a5a11baa1d129b92ce5",
+    "url": "/Runner_Ninja/static/media/Jump__000.069af838.png"
+  },
+  {
+    "revision": "2297b4da221b4a34b858f25290ce14b2",
+    "url": "/Runner_Ninja/static/media/Jump__001.2297b4da.png"
+  },
+  {
+    "revision": "26074aac1032289f0ebf2a0aa3a82f72",
+    "url": "/Runner_Ninja/static/media/Jump__002.26074aac.png"
+  },
+  {
+    "revision": "d87fbda50f1f63333c12129fdc39d1eb",
+    "url": "/Runner_Ninja/static/media/Jump__006.d87fbda5.png"
+  },
+  {
+    "revision": "854a8ee9c675ef34a244e03e67d3051b",
+    "url": "/Runner_Ninja/static/media/Jump__007.854a8ee9.png"
+  },
+  {
+    "revision": "928ab5d541585aca4ee9fae490625c77",
+    "url": "/Runner_Ninja/static/media/Jump__009.928ab5d5.png"
+  },
+  {
+    "revision": "e815c389bd8fc99640dd0a6ebca9535e",
+    "url": "/Runner_Ninja/static/media/Run__000.e815c389.png"
+  },
+  {
+    "revision": "02daf007150b9f31eaab7d042ed48e5a",
+    "url": "/Runner_Ninja/static/media/Run__001.02daf007.png"
+  },
+  {
+    "revision": "3fbd16f2b86f9b00a6d5aecf34480c8a",
+    "url": "/Runner_Ninja/static/media/Run__002.3fbd16f2.png"
+  },
+  {
+    "revision": "ef79f5ab58bf19d81eb434eb76f608ca",
+    "url": "/Runner_Ninja/static/media/Run__003.ef79f5ab.png"
+  },
+  {
+    "revision": "20348cbf0ebf2b9fe95aa17d31295782",
+    "url": "/Runner_Ninja/static/media/Run__004.20348cbf.png"
+  },
+  {
+    "revision": "ae3c8f6861c7d0ebe38b710cf782d2f5",
+    "url": "/Runner_Ninja/static/media/Run__005.ae3c8f68.png"
+  },
+  {
+    "revision": "a40039fbe7de774b4fb3cdd42c03f6ac",
+    "url": "/Runner_Ninja/static/media/Run__006.a40039fb.png"
+  },
+  {
+    "revision": "017f25b8ace4911bb9338a7b246036c9",
+    "url": "/Runner_Ninja/static/media/Run__007.017f25b8.png"
+  },
+  {
+    "revision": "1022fb8f0ca4c0c939d92caffeca6829",
+    "url": "/Runner_Ninja/static/media/Run__008.1022fb8f.png"
+  },
+  {
+    "revision": "0815a96abfe53730f0c778a5f3da71dc",
+    "url": "/Runner_Ninja/static/media/Run__009.0815a96a.png"
+  },
+  {
+    "revision": "c202bdb24dd0ec06bb80e90caa2d5bfa",
+    "url": "/Runner_Ninja/static/media/Throw__004.c202bdb2.png"
+  },
+  {
+    "revision": "e5ae92f6d6610f33503080ab3712404f",
+    "url": "/Runner_Ninja/static/media/daydreamer.e5ae92f6.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Runner_Ninja/precache-manifest.de7dec3818d740bf3a1b4d159db4980f.js"
+  "/Runner_Ninja/precache-manifest.9e2c5e270e602b6f6513579b7bd7cace.js"
 );
 
 self.addEventListener('message', (event) => {
